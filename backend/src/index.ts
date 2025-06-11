8fc646f0cb812ef87f4fa1fea605bf94d5421534
rsr077 <yourfriend77777777@gmail.com>
2025-06-11 12:48:58 +0530

    Init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,36 +1,65 @@
-import { Hono } from 'hono';
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign } from 'hono/jwt';
-
+import { Hono } from 'hono';
+import { sign , verify } from 'hono/jwt'
+import { initMiddleware } from './middleware';
 
 // Create the main Hono app
 const app = new Hono();
 
-app.post('/api/v1/signup', async(c) => {
+initMiddleware(app)
+
+
+
+
+
+app.post('/api/v1/signup', async (c) => {
 	const prisma = new PrismaClient({
-		//@ts-ignore  
-    datasourceUrl: c.env.DATABASE_URL,
-}).$extends(withAccelerate())
+    //@ts-ignore
+		datasourceUrl: c.env.DATABASE_URL	,
+	}).$extends(withAccelerate()); 
 
-const body = await c.req.json();
-const user =  await prisma.user.create({
-	data: {
-		email: body.email,
-		password: body.password,  
-	},
-})
-  	//@ts-ignore  
-const token = sign({id: user.id}, c.env.JWT_SECRET)
-	return c.json({
-		jwt: token,
-})
-}) 
+	const body = await c.req.json();
 
-app.post('/api/v1/signin', (c) => {
-	return c.text('signin route') 
+		const user = await prisma.user.create({
+			data: {
+				email: body.email,
+				password: body.password
+			},
+		})
+		   //@ts-ignore
+    const token = await sign({ id: user.id },c.env.JWT_SECRET)
+
+    return c.json ({
+      jwt: token
+    })
+
+  
 })
 
+app.post('/api/v1/sigin', async (c) => {
+	const prisma = new PrismaClient({
+    //@ts-ignore
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate()); 
+
+	const body = await c.req.json();
+
+		const user = await prisma.user.findUnique({
+			where: {
+				email: body.email,
+				password: body.password
+			},
+		})
+		   //@ts-ignore
+    const token = await sign({ id: user.id },c.env.JWT_SECRET)
+
+    return c.json ({
+      jwt: token
+    })
+
+  
+})
 app.get('/api/v1/blog/:id', (c) => {
 	const id = c.req.param('id')
 
---
+++ backend/src/middleware.ts
@@ -0,0 +1,30 @@
+
+
+export function initMiddleware(app:any) {
+ 
+	     //@ts-ignore
+       
+ app.use('api/v1/blog/*', async (c, next) => {
+  
+    const header = c.req.header('Authorization') || "";
+  
+    const token = header.split(' ')[1]; 
+    // get the header 
+    // verify the header 
+    // if the headr is correct, we need can proceed 
+    // if not, we return the user a 403 status code 
+  
+    //@ts-ignore
+  
+    const response = await verify(header,c.env.JWT_SECRET)
+    if(response.id) {
+      await next();
+    }else {
+      c.status(403)
+      return c.json({ error: 'Unauthorized' });
+    }
+  
+    })
+
+
+}
