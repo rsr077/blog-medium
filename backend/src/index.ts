48d9684a0aa3b47722e87e829f03a19c500ae736
rsr077 <yourfriend77777777@gmail.com>
2025-06-12 08:29:34 +0530

    Init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,62 +4,65 @@ import { Hono } from 'hono';
 import { sign , verify } from 'hono/jwt'
 import { initMiddleware } from './middleware';
 
-// Create the main Hono app
-const app = new Hono();
-
-initMiddleware(app)
-
-
-
+/// Create the main Hono app
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string,
+		JWT_SECRET: string,
+	}
+}>();
 
 
 app.post('/api/v1/signup', async (c) => {
 	const prisma = new PrismaClient({
-    //@ts-ignore
-		datasourceUrl: c.env.DATABASE_URL	,
-	}).$extends(withAccelerate()); 
+		datasourceUrl: c.env?.DATABASE_URL	,
+	}).$extends(withAccelerate());
 
 	const body = await c.req.json();
-
+	try {
 		const user = await prisma.user.create({
 			data: {
 				email: body.email,
 				password: body.password
-			},
-		})
-		   //@ts-ignore
-    const token = await sign({ id: user.id },c.env.JWT_SECRET)
-
-    return c.json ({
-      jwt: token
-    })
-
-  
+			}
+		});
+		const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+		return c.json({ jwt });
+	} catch(e) {
+		c.status(403);
+		return c.json({ error: "error while signing up" });
+	}
 })
 
-app.post('/api/v1/sigin', async (c) => {
+
+
+
+app.post('/api/v1/signin', async (c) => {
 	const prisma = new PrismaClient({
-    //@ts-ignore
 		datasourceUrl: c.env?.DATABASE_URL	,
-	}).$extends(withAccelerate()); 
+	}).$extends(withAccelerate());
 
 	const body = await c.req.json();
+	const user = await prisma.user.findUnique({
+		where: {
+			email: body.email,
+			password: body.password
+		}
+	});
 
-		const user = await prisma.user.findUnique({
-			where: {
-				email: body.email,
-				password: body.password
-			},
-		})
-		   //@ts-ignore
-    const token = await sign({ id: user.id },c.env.JWT_SECRET)
+	if (!user) {
+		c.status(403);
+		return c.json({ error: "user not found" });
+	}
 
-    return c.json ({
-      jwt: token
-    })
-
-  
+	const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+	return c.json({ jwt });
 })
+
+
+
+
+
 app.get('/api/v1/blog/:id', (c) => {
 	const id = c.req.param('id')
 
@@ -70,6 +73,12 @@ app.post('/api/v1/blog', (c) => {
 
 	return c.text('signin route')
 })
+
+app.put('/api/v1/blog', (c) => {
+	return c.text('signin route')
+})
+
+
 
 app.put('/api/v1/blog', (c) => {
 	return c.text('signin route')
